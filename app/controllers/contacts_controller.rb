c7d31341bb24e37c27c98d91bc17509e64635422
markprubin <mark.p.rubin@gmail.com>
2022-11-01 12:30:18 -0700

    adds all_contacts path and action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,4 +3,9 @@ class ContactsController < ApplicationController
     contact = Contact.first
     render json: { id: contact.id, first_name: contact.first_name, last_name: contact.last_name, email: contact.email, phone_number: contact.phone_number}
   end
+
+  def all_contacts
+    contacts = Contact.all
+    render json: contacts.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/one_contact", controller: "contacts", action:"one_contact"
+  get "/all_contacts" => "contacts#all_contacts"
 end
